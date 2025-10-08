bcf1e81f6b5e5cea996f838d119fc14f1892f265
Miguel Araujo <mickey.araujo@yahoo.com.mx>
2025-10-07 22:44:38 -0600

    Tercer script

---
+++ Estadisticos.R
@@ -0,0 +1,4 @@
+mis_numeros <- c(10, 12, 15, 11, 18, 20, 14)
+summary(mis_numeros) # Obtiene un resumen de los datos
+mean(mis_numeros)    # Calcula la media
+sd(mis_numeros)      # Calcula la desviación estándar
